8a06100997a66d7b00d6223718efc04cf42377c7
dlashyang <dlashyang@gmail.com>
2020-02-20 17:01:48 +0800

    implement read_from_file

--- pmp_process.py
+++ pmp_process.py
@@ -13,7 +13,29 @@ class pmp_object(object):
             print ("pmp_type error: " % pmp_type)
 
 def read_from_file(file_name, proc_dict, file_dict):
-    pass
+    proc_name = ''
+    with open( file_name, 'r' ) as f:
+        for line in f:
+            if ('[process]' in line):
+                proc_name=line.replace('[process]','')
+                if not (proc_name in proc_dict.keys()):
+                    proc_dict[proc_name]=pmp_object(proc_name)
+            elif ('[input]' in line):
+                input_files=line.replace('[input]','').split(';')
+                for in_file in input_files:
+                    if not (in_file in file_dict.keys()):
+                        file_dict[in_file]=pmp_object(in_file)
+                    proc_dict[proc_name].add('in',file_dict[in_file])
+                    file_dict[in_file].add('up',proc_dict[proc_name])
+            elif ('[output]' in line):
+                output_files=line.replace('[output]','').split(';')
+                for out_file in output_files:
+                    if not (out_file in file_dict.keys()):
+                        file_dict[out_file]=pmp_object(out_file)
+                    proc_dict[proc_name].add('out',file_dict[out_file])
+                    file_dict[out_file].add('down',proc_dict[proc_name])
+            else:
+                proc_name=''
 
 def go_through_proc(first_proc, proc_dict):
     pass
